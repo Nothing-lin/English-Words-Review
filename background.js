ed2b1b88ef402b66e17ce71cbfed147e54c171a9
nothing-lin <1353695200@qq.com>
2021-05-19 17:46:38 +0800

    以数组的形式获取xml生词库中的单词，并遍历循环到一个数组变量中，在console中输出

--- background.js
+++ background.js
@@ -12,12 +12,20 @@ function load_english_words() {
   words_file_path = chrome.extension.getURL("resource/words_demo.xml");
   //定义异步数据请求
   var xhr = new XMLHttpRequest();
+  //调用xml单词文件
   xhr.open('GET', words_file_path, true);
-
-  xhr.onreadystatechange = function(){
-    if(xhr.readyState == 4 && xhr.status == 200){
+//如果成功调用xhr.open()函数的话就执行下面的函数
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      //info获取xml文件中的全部内容，并保存xml的全部内容和格式
       var info = xhr.responseXML;
-      var words = info.getElementsByTagName("word")[1].textContent;
+      var words = [];
+      //遍历循环word标签内容到数组words中
+      for (var i = 0; i < 2; i++) {
+        word = info.getElementsByTagName("word")[i].textContent;
+        words[i] = word;
+      }
+      // var words = info.getElementsByTagName("word")[1].textContent;
       console.log(words);
     }
   }
